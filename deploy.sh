961627693a24b865a15332b97e4686b2fd854985
stshrive <stshrive@mit.edu>
2019-01-24 08:38:46 -0500

    Create a duplicate of the local execute command for remote deploments

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+finish()
+{
+    popd > /dev/null
+    unset -f finish
+}
+
+pushd $(dirname $0) > /dev/null
+
+trap finish SIGINT SIGTERM
+
+# dockerfile is expected to be in the same directory
+docker build . --build-arg USER_ID=$(id -u $USER) -t pynb-cloud
+
+# Need to determine how to push to a container registry within
+# each cloud platform using their CLI?
+
+# Ideally this is created on the fly and the required login credentials
+# can be used to use a docker login command and push to the new registry.
+# After the container is deployed we launch a deployment for the app
+# using the container we just uploaded to the registry and it runs remotely.
+
+while IFS= read -r line; do
+    echo "$line" | perl ./ipynb-url -
+done < <(docker run -i -p 8888:8888 pynb-cloud 2>&1)
+
+finish
