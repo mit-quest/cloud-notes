6c9383bd8ae4bfc88aee9626827eded43647fe31
stshrive <stshrive@mit.edu>
2019-01-24 08:35:07 -0500

    Make sure script is running in the correct directory and traps SIGINT

--- local-execute.sh
+++ local-execute.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+finish()
+{
+    popd > /dev/null
+    unset -f finish
+}
+
+pushd $(dirname $0) > /dev/null
+
+trap finish SIGINT SIGTERM
+
+# dockerfile is expected to be in the same directory
 docker build . --build-arg USER_ID=$(id -u $USER) -t pynb-cloud
 
 while IFS= read -r line; do
     echo "$line" | perl ./ipynb-url -
 done < <(docker run -i -p 8888:8888 pynb-cloud 2>&1)
+
+finish
