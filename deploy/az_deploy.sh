b261bb5ae7b7e3022ca5baca5f5e414cf2167487
stshrive <stshrive@mit.edu>
2019-01-28 12:15:09 -0500

    Add docker tag and push commands

--- deploy/az_deploy.sh
+++ deploy/az_deploy.sh
@@ -5,6 +5,8 @@ docker pull microsoft/azure-cli
 
 CONFIG=azure-config
 LOCATION=eastus
+RG=qi-bridge-transient-deploy-rg
+REGISTRY=qitransientregistry
 
 SUBSCRIPTION_ID=$1
 
@@ -21,5 +23,16 @@ AZ="docker run --rm -it -e AZURE_CONFIG_DIR=/$CONFIG --volumes-from $CONFIG micr
 
 $AZ account set -s $SUBSCRIPTION_ID
 
-# Should check for group existence here using `az group list` before group creation.
-$AZ group create --name qi-bridge-transient-deploy-rg --location $LOCATION
+# Should check for group existence here using `az group list`
+# before clling `az group create [params]`
+$AZ group create --name $RG --location $LOCATION
+
+$AZ acr create --resource-group $RG --name $REGISTRY --sku Basic
+
+# Can't use `az acr login` from docker container. This command requires
+# an installation of docker to exist as it calls `docker ps` internally
+# and then issues a `docker login` command
+# $AZ acr login --name $REGISTRY
+
+docker tag pynb-cloud ${REGISTRY}.azurecr.io/jupyter-server:deployment
+docker push ${REGISTRY}.azurecr.io/jupyter-server:deployment
