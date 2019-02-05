2af82cea302363a7e23f163ebe1344daac068a4a
stshrive <stshrive@mit.edu>
2019-02-05 09:41:51 -0500

    Add application variable

--- local-execute.sh
+++ local-execute.sh
@@ -2,9 +2,12 @@
 
 set -e
 
+APPLICATION="cloud-notes"
+
 finish()
 {
     popd > /dev/null
+    unset APPLICATION
     unset -f finish
 }
 
@@ -12,11 +15,12 @@ pushd $(dirname $0) > /dev/null
 
 trap finish SIGINT SIGTERM
 
+
 # dockerfile is expected to be in the same directory
-docker build . --build-arg USER_ID=$(id -u $USER) -t pynb-cloud
+docker build . --build-arg USER_ID=$(id -u $USER) -t $APPLICATION
 
 while IFS= read -r line; do
     echo "$line" | perl ./ipynb-url -
-done < <(docker run -i -p 8888:8888 pynb-cloud 2>&1)
+done < <(docker run -i -p 8888:8888 $APPLICATION 2>&1)
 
 finish
