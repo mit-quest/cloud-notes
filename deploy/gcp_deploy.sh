b4941130b0866bac5cfff0ccd0d2b6247053f78a
stshrive <stshrive@mit.edu>
2019-02-05 17:21:15 -0500

    Add dynamic resource creation to GCP (to match azure)

--- deploy/gcp_deploy.sh
+++ deploy/gcp_deploy.sh
@@ -1,44 +1,70 @@
 #!/bin/bash
 
-# TODO: ask for this ID
-PROJECT_ID=$1
-
+LOCATION="us-east4-a"
 CLUSTER=qitransientcluster
-
-# Providing a default
-ZONE="us-east4-a"
+REG_SERVER=gcr.io/${RESOURCES}
 
 # Use the Prebuilt gcloud sdk container provided by Google.
 docker pull google/cloud-sdk
 
 # Login with the container. This will likely require user input.
-docker run -it --name gcloud-config google/cloud-sdk gcloud auth login
+docker run \
+    -it \
+    --name gcloud-config \
+    google/cloud-sdk \
+    gcloud auth login
 
 # Common rerun commands for gcloud commands inside of docker image
 PREFIX="docker run --rm -it --volumes-from gcloud-config google/cloud-sdk"
 GCLOUD="$PREFIX gcloud"
 KUBCTL="$PREFIX kubctl"
 
-# Set the project to the provided project ID
-$GCLOUD config set project $PROJECT_ID
+# Create a hosting project for transient resources.
+$GCLOUD projects create $RESOURCES
 
-# Enable the use of the container registry gcr.io
+$GCLOUD config set project $RESOURCES
+$GCLOUD config set compute/zone $LOCATION
+
+LIST_BILLING="$GCLOUD beta billing accounts list --filter open=true"
+LIST_FORMAT_DISPLAY="\"list[no-heading](name, displayName)\""
+LIST_FORMAT_HIDDEN="\"list[no-heading](name)\""
+
+let option_id=0
+while IFS= read -r line; do
+    let option_id++
+    echo "[${option_id}] ${line}"
+done < <($GCLOUD_BILLING $LIST_FORMAT_DISPLAY)
+
+read -n 1 -p "CHOOSE A BILLING ACCOUNT TO LINK WITH ${RESOURCES}:"
+
+let option_id=0
+while IFS= read -r line; do
+    let option_id++
+    if [ "$option_id" = $CHOICE ]; then
+        $GCLOUD alpha billing projects link $RESOURCES --account-id=$line
+        break
+    fi
+done < <($GCLOUD_BILLING $LIST_FORMAT_HIDDEN)
+
+# Enable the use of the container and the container registry APIs for project
 $GCLOUD services enable containerregistry.googleapis.com
+$GCLOUD services enable container.googleapis.com
 
 # Add authentication information for docker to login to gcr.io
-$GCLOUD auth configure-docker --project $PROJECT_ID
+$GCLOUD auth configure-docker --project $RESOURCES
 
 # Login to gcr.io
-$GCLOUD auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://gcr.io
-
-GCR_IMAGE=gcr.io/${PROJECT_ID}/${APPLICATION}:deployment
+$GCLOUD auth print-access-token | docker login \
+    -u oauth2accesstoken \
+    --password-stdin https://gcr.io
 
 # Tag and push the image to the new gcr.io repository
-docker tag ${APPLICATION} ${GCR_IMAGE}
-docker push ${GCR_IMAGE}
+docker tag ${APPLICATION} ${REG_SERVER}/${CR_IMAGE}
+docker push ${REG_SERVER}/${CR_IMAGE}
 
 # Create a cluster to run the container
-$GCLOUD clusters create $CLUSTER --num-nodes 1
+$GCLOUD container clusters delete $CLUSTER > /dev/null
+$GCLOUD container clusters create $CLUSTER --num-nodes 1
 
-$KUBECTL run ${APPLICATION} --image ${GCR_IMAGE} --port 8888
+$KUBECTL run ${APPLICATION} --image=${REG_SERVER}/${CR_IMAGE} --port=8888
 $KUBECTL expose deployment ${APPLICATION} --port 8888 --target-port 8888
