abe2b121d7cbcae7916f7a9bb3122b79d2ba6d53
stshrive <stshrive@mit.edu>
2019-02-05 12:01:13 -0500

    Use a bind mount to enable saving changes to local machine

--- local-execute.sh
+++ local-execute.sh
@@ -12,15 +12,20 @@ finish()
 }
 
 pushd $(dirname $0) > /dev/null
+export MOUNTSOURCE=$(pwd)
 
 trap finish SIGINT SIGTERM
-
 
 # dockerfile is expected to be in the same directory
 docker build . --build-arg USER_ID=$(id -u $USER) -t $APPLICATION
 
 while IFS= read -r line; do
     echo "$line" | perl ./ipynb-url -
-done < <(docker run -i -p 8888:8888 $APPLICATION 2>&1)
+done < <(docker run \
+    --rm \
+    -i \
+    -p 8888:8888 \
+    --mount type=bind,source="${MOUNTSOURCE}/workspace",target="/workspace" \
+    $APPLICATION 2>&1)
 
 finish
