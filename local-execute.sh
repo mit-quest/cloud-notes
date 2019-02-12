99c2f83ef7c2805c084b2e9c4124a7ba8ee7e4c8
stshrive <stshrive@mit.edu>
2019-02-12 17:29:36 -0500

    Trap exit signal

--- local-execute.sh
+++ local-execute.sh
@@ -14,7 +14,7 @@ finish()
 pushd $(dirname $0) > /dev/null
 export MOUNTSOURCE=$(pwd)
 
-trap finish SIGINT SIGTERM
+trap finish SIGINT SIGTERM EXIT
 
 # dockerfile is expected to be in the same directory
 docker build . --build-arg USER_ID=$(id -u $USER) -t $APPLICATION
@@ -27,5 +27,3 @@ done < <(docker run \
     -p 8888:8888 \
     --mount type=bind,source="${MOUNTSOURCE}/workspace",target="/workspace" \
     $APPLICATION 2>&1)
-
-finish
