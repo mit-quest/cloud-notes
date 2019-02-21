005897e8e2df2c31d113fbd3753a386e609412f9
stshrive <stshrive@mit.edu>
2019-02-21 11:20:25 -0500

    Add utility function to check for WSL environment

---
+++ bin/utils.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function IsWindows()
+{
+    if grep -qE "(Microsoft|WSL)" /proc/version &> /dev/null; then
+        echo true
+    else
+        echo false
+    fi
+
+    return 0
+}
