59d7bd3ff71c38a70c1ee2bf183df0439b2db75c
stshrive <stshrive@mit.edu>
2019-02-25 14:40:32 -0500

    Move PushToRemote to deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+# Tags and pushes an image to a remote registry.
+# ARGUMENTS:
+#   LOCAL_IMAGE - Docker image name on the local machine
+#   REGISTRY    - The remote Container Registry URL
+#.
+# Returns: the remote image name as REMOTE_IMAGE.
+#
+function PushToRemote()
+{
+    LOCAL_IMAGE=$1
+    REGISTRY=$2
+
+    REMOTE_IMAGE="${REGISTRY}/${LOCAL_IMAGE}:dep.$(id -u -n $RUID)"
+
+    docker tag ${LOCAL_IMAGE} ${REMOTE_IMAGE} > /dev/null
+    docker push ${REMOTE_IMAGE} > /dev/null
+
+    echo ${REMOTE_IMAGE}
+}
+
+export -f PushToRemote
--- run.sh
+++ run.sh
@@ -44,28 +44,6 @@ else
     export JUPYTER_SERVER=""
 fi
 
-# Tags and pushes an image to a remote registry.
-# ARGUMENTS:
-#   LOCAL_IMAGE - Docker image name on the local machine
-#   REGISTRY    - The remote Container Registry URL
-#.
-# Returns: the remote image name as REMOTE_IMAGE.
-#
-function PushToRemote()
-{
-    LOCAL_IMAGE=$1
-    REGISTRY=$2
-
-    REMOTE_IMAGE="${REGISTRY}/${LOCAL_IMAGE}:dep.$(id -u -n $RUID)"
-
-    docker tag ${LOCAL_IMAGE} ${REMOTE_IMAGE} > /dev/null
-    docker push ${REMOTE_IMAGE} > /dev/null
-
-    echo ${REMOTE_IMAGE}
-}
-
-export -f PushToRemote
-
 function contains()
 {
     local element match="$1"
