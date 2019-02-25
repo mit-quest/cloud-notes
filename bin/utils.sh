ba91348b463c231d26137c66da224c7e2556ef01
stshrive <stshrive@mit.edu>
2019-02-25 17:54:19 -0500

    Refactor provider checking

--- bin/utils.sh
+++ bin/utils.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Checks for Docker installation and causes script to exit
+# if installation cannot be found.
+#
 function RequireDocker()
 {
     if [ ! -x "$(command -v docker)" ]; then
@@ -8,43 +11,71 @@ function RequireDocker()
    fi
 }
 
+# Get the absolute path to the provided script
+# ARGUMENTS:
+#   _FILE_NAME - The name of the file or script to find
+#                  the absolute filesystem path for
+# RETURNS:
+#   The absolute path to the file as _FILE_PATH
 function GetAbsPath()
 {
-    _SCRIPT_NAME=$1
+    _FILE_NAME=$1
 
-    # Get the absolute path to the working directory
-    pushd $(dirname $_SCRIPT_NAME) > /dev/null
-    _SCRIPT_DIR=$(pwd -P)
+    pushd $(dirname $_FILE_NAME) > /dev/null
+    _FILE_PATH=$(pwd -P)
     popd > /dev/null
 
-    echo $_SCRIPT_DIR
+    echo $_FILE_PATH
 }
 
+# Valid Platforms/Providers
 PROVIDERS=(aws gcp az ibm local)
-ARGUMENTS="please specify ONE of the following providers:[${PROVIDERS[*]}]"
 
-if [ $# -ne 1 ]
-then
-    echo "arguments for $0: <CLOUD_PROVIDER>"
-    echo $ARGUMENTS
+# Checks for a value givrn the contents of an Array
+#
+# ARGUMENTS:
+#   _VALUE    - The value to look for in an array
+#   _ELEMENTS - The array to look through for _VALUE
+# RETURNS:
 
-    exit 1
-fi
-
-function contains()
+function Contains()
 {
-    local element match="$1"
+    # _ELEMENT the current item from _ELEMENTS
+    # _VALUE the item to find in _ELEMENTS
+    local _ELEMENT _VALUE="$1"
     shift
-    for element
+
+    for _ELEMENT
     do
-        if [[ "$element" == "$match" ]]; then
-            echo "SUCCESS"
+        if [[ "$_VALUE" == "$_ELEMENT" ]]; then
+            # 0 == true
             return 0
         fi
     done
-    echo "ERROR"
+
+    # 1 == false
     return 1
 }
+
+# Checks the supplied provider against a list of supported providers
+# ARGUMENTS:
+#   PROVIDER - The Cloud platform provider name.
+#
+function CheckProvider()
+{
+    local _PROVIDER = $1
+    local ERROR_MSG="please specify ONE of the following providers:[${PROVIDERS[*]}]"
+
+    if [ $# -ne 1 ]
+    then
+        echo "arguments for $0: <CLOUD_PROVIDER>"
+        echo $ARGUMENTS
+
+        exit 1
+    fi
+}
+
+
 
 
 function IsWindows()
