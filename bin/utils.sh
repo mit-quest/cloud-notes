183851abb9dcdba063553b82f1f43cf3cdc86634
stshrive <stshrive@mit.edu>
2019-02-25 14:49:50 -0500

    SImplify docker installation message and redirect to stderr

--- bin/utils.sh
+++ bin/utils.sh
@@ -3,9 +3,7 @@
 function RequireDocker()
 {
     if [ ! -x "$(command -v docker)" ]; then
-        echo "\
-An installation of docker is required. \
-See https://docs.docker.com/install for installation instructions."
+        echo "Docker is not installed." 1>&2
         exit 1
    fi
 }
