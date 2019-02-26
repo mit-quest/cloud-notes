e348ac459cf85eaf52f1543a04db919a97268dc5
stshrive <stshrive@mit.edu>
2019-02-25 22:24:00 -0500

    Move variables to deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -21,3 +21,7 @@ function PushToRemote()
 }
 
 export -f PushToRemote
+
+export CONFIG_MOUNT=/.persistant_data
+export RESOURCES="$(id -u -n $RUID)-transient-resources"
+export CONTAINER_NAME="${PROVIDER}-config"
