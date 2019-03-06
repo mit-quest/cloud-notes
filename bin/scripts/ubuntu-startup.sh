e47185e2bbef72c4816a06b2b6417c622d3c424e
stshrive <stshrive@mit.edu>
2019-03-05 19:33:33 -0500

    move startup script to file

---
+++ bin/scripts/ubuntu-startup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+echo "Checking for CUDA and installing."
+# Check for CUDA and try to install.
+if ! dpkg-query -W cuda-10-0; then
+    curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+    dpkg -i ./cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+    apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+    apt-get update
+    apt-get install cuda-10-0 -y
+fi
+
+# Enable persistence mode
+nvidia-smi -pm 1
+
+echo Checking for Docker and installing
+# Check for docker and try to install.
+if ! dpkg-query -W docker-ce; then
+    apt-get update
+    apt-get install \
+        apt-transport-https \
+        ca-certificates \
+        gnupg-agent \
+        software-properties-common
+
+    curl -fsSL https://download.docker.com/linux/linux/ubuntu/gpg | apt-key add -
+
+    apt-add-repository \
+        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) \
+        stable"
+
+    apt-get update
+    apt-get install -y \
+        docker-ce \
+        docker-ce-cli \
+        containerd.io
+
+    gcloud auth configure-docker
+    gcloud auth print-access-token | docker login \
+        -u oauth2accesstoken \
+        --passwd-stdin \
+        <REGISTRY>
+fi
