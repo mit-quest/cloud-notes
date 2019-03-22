c5a65d8f31b464dfcf2ce03d43cbbbabddbdb439
stshrive <stshrive@mit.edu>
2019-03-22 16:55:55 -0400

    Fix multi-line if statement

--- bin/win_utils.ps1
+++ bin/win_utils.ps1
@@ -15,15 +15,15 @@ function NeedsRestart
 function __QI_ActivateOptionalWindowsSettings
 {
   if (
-    (Enable-WindowsOptionalFeature -Online -FeatureName Containers | NeedsRestart)
-    -or (Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All | NeedsRestart)
-    -or (Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V | NeedsRestart)
-    -or (Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Tools-All | NeedsRestart)
-    -or (Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Management-Powershell | NeedsRestart)
-    -or (Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Hypervisor | NeedsRestart)
-    -or (Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Services | NeedsRestart)
-    -or (Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Management-Clients | NeedsRestart)
-    -or (Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux | NeedsRestart)
+    (Enable-WindowsOptionalFeature -Online -FeatureName Containers | NeedsRestart) -or
+    (Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All | NeedsRestart) -or
+    (Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V | NeedsRestart) -or
+    (Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Tools-All | NeedsRestart) -or
+    (Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Management-Powershell | NeedsRestart) -or
+    (Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Hypervisor | NeedsRestart) -or
+    (Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Services | NeedsRestart) -or
+    (Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-Management-Clients | NeedsRestart) -or
+    (Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux | NeedsRestart)
     ){
       return $false
   }
