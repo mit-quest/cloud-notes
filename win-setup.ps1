e9b3fc31544d6c029ee235e0f805cc7529533fc0
stshrive <stshrive@mit.edu>
2019-03-22 15:47:40 -0400

    Add WSL install if no restart is required

--- win-setup.ps1
+++ win-setup.ps1
@@ -11,13 +11,18 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 RefreshEnv.cmd
 
-Write-Host "Installing Docker for Windows"
-choco install docker-for-windows -y
+Write-Host "Installing Docker Desktop"
+choco install docker-desktop -y
 
 Write-Host "Turning on optional features for container execution and WSL"
-__QI_ActivateOptionalWindowsSettings
-
-# Get the registry key for RunOnce following the currently executing users
-# registry path.
-$RunOnce = "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
-Set-ItemProperty $RunOnce "NextRun" ("C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -executionPolicy Unrestricted -File $RestartScript")
+if (__QI_ActivateOptionalWindowsSettings)
+{
+  __QI_WSLInstall
+}
+else
+{
+  # Get the registry key for RunOnce following the currently executing users
+  # registry path.
+  $RunOnce = "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
+  Set-ItemProperty $RunOnce "NextRun" ("C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -executionPolicy Unrestricted -File $RestartScript")
+}
